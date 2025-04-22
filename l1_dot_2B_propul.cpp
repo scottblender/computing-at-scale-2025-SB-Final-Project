60a4e1565ef545b5f2157f9606381029c9237327
scottblender <57569396+scottblender@users.noreply.github.com>
2025-04-22 15:57:36 -0400

    Add files via upload

---
+++ l1_dot_2B_propul.cpp
@@ -0,0 +1,56 @@
+#include "l1_dot_2B_propul.hpp"
+#include <Eigen/Dense>
+#include <cmath>
+
+Eigen::VectorXd l1_dot_2B_propul(
+    double T, double F, double G, double H, double K, double L,
+    double P, double thrust, double g0,
+    double l_F, double l_G, double l_H, double l_K,
+    double l_L, double l_P, double m, double m0, double mu
+) {
+    double t2 = 1.0 / mu;
+    double t3 = std::cos(L);
+    double t4 = P * t2;
+    double t5 = std::sqrt(t4);
+    double t7 = 1.0 / t5;
+    double t8 = std::sin(L);
+    double t9 = H * t8;
+    double t10 = F * t3;
+    double t11 = G * t8;
+    double t12 = t10 + t11 + 1.0;
+    double t13 = 1.0 / t12;
+    double t31 = K * t3;
+    double t14 = t9 - t31;
+    double t15 = F * t3 * 2.0;
+    double t16 = G * t8 * 2.0;
+    double t17 = t15 + t16 + 2.0;
+    double t18 = 1.0 / t17;
+    double t19 = H * H;
+    double t20 = K * K;
+    double t21 = t19 + t20 + 1.0;
+
+    double t32 = l_H * t3 * t7 * t18 * t21;
+    double t33 = G * l_F * t7 * t13 * t14;
+    double t34 = l_K * t7 * t8 * t18 * t21;
+    double t22 = std::abs(t32 - t33 + t34 + l_L * t7 * t13 * t14 + F * l_G * t7 * t13 * t14);
+
+    double t36 = l_G * t3 * t7;
+    double t37 = l_F * t7 * t8;
+    double t23 = std::abs(t36 - t37);
+
+    double t28 = G + t8;
+    double t29 = t13 * t28;
+    double t30 = t8 + t29;
+
+    double t40 = F + t3;
+    double t41 = t13 * t40;
+    double t42 = t3 + t41;
+
+    double t43 = l_F * t7 * t42;
+    double t44 = l_G * t7 * t30;
+
+    Eigen::VectorXd out(6);
+    out << -t22, -t23, -t22, -t22, -t22, -(t43 + t44);
+
+    return out;
+}
---
+++ lm_dot_2B_propul.cpp
@@ -0,0 +1,52 @@
+#include "lm_dot_2B_propul.hpp"
+#include <cmath>
+
+double lm_dot_2B_propul(
+    double T, double F, double G, double H, double K, double L,
+    double P, double thrust, double g0,
+    double l_F, double l_G, double l_H, double l_K,
+    double l_L, double l_P, double m, double m0, double mu
+) {
+    double t2 = 1.0 / mu;
+    double t3 = P * t2;
+    double t4 = std::sqrt(t3);
+    double t5 = t4;
+    double t6 = std::sin(L);
+    double t7 = std::cos(L);
+    double t8 = H * t6;
+    double t9 = F * t7;
+    double t10 = G * t6;
+    double t11 = t9 + t10 + 1.0;
+    double t12 = 1.0 / t11;
+    double t32 = K * t7;
+    double t13 = t8 - t32;
+    double t14 = F * t7 * 2.0;
+    double t15 = G * t6 * 2.0;
+    double t16 = t14 + t15 + 2.0;
+    double t17 = 1.0 / t16;
+    double t18 = H * H;
+    double t19 = K * K;
+    double t20 = t18 + t19 + 1.0;
+
+    double t33 = l_H * t5 * t7 * t17 * t20;
+    double t34 = G * l_F * t5 * t12 * t13;
+    double t35 = l_K * t5 * t6 * t17 * t20;
+    double t21 = std::abs(t33 - t34 + t35 + l_L * t5 * t12 * t13 + F * l_G * t5 * t12 * t13);
+
+    double t22 = l_G * t5 * t7;
+    double t37 = l_F * t5 * t6;
+    double t23 = t22 - t37;
+
+    double t29 = F + t7;
+    double t30 = t12 * t29;
+    double t31 = t7 + t30;
+
+    double t39 = l_F * t5 * t31;
+    double t40 = G + t6;
+    double t41 = t12 * t40;
+    double t42 = t6 + t41;
+
+    double t43 = l_G * t5 * t42;
+
+    return -(t21 + std::abs(t23) + t39 + t43);
+}
