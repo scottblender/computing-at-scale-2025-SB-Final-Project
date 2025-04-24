a839e1370b57638ae7bbdb3c720bf953b36ab79c
Scott Blender <scottblender20@gmail.com>
2025-04-24 19:36:45 -0400

    Fixing the basics

--- test_sigma_propagation_2.cpp
+++ test_sigma_propagation_2.cpp
@@ -1,5 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>  
 #include <Kokkos_Core.hpp>
 #include <Eigen/Dense>
 #include <fstream>
@@ -15,10 +15,10 @@ TEST_CASE("Check if first row in expected CSV corresponds to bundle 32, sigma 0"
     const int expected_bundle = 32;
     const int expected_sigma = 0;
 
-    int bundle = static_cast<int>(expected(0, 0));
-    int sigma = static_cast<int>(expected(0, 1));
+    double bundle = expected(0, 0);
+    double sigma = expected(0, 1);
 
     INFO("First row values: bundle = " << bundle << ", sigma = " << sigma);
-    CHECK_THAT(bundle, Catch::Matchers::Equals(expected_bundle));
-    CHECK_THAT(sigma, Catch::Matchers::Equals(expected_sigma));
+    CHECK_THAT(bundle, Catch::Matchers::WithinAbs(static_cast<double>(expected_bundle), 1e-8));
+    CHECK_THAT(sigma, Catch::Matchers::WithinAbs(static_cast<double>(expected_sigma), 1e-8));
 }
