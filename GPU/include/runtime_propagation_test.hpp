a4b6ec9be0763a1f0aacb1b85c99e24403e3b850
Scott Blender <scottblender20@gmail.com>
2025-04-28 20:47:25 -0400

    Will it work

--- GPU/include/runtime_propagation_test.hpp
+++ GPU/include/runtime_propagation_test.hpp
@@ -86,6 +86,7 @@ inline double run_propagation_test(int num_steps, const PropagationSettings& set
     // Copy data from host to device
     Kokkos::deep_copy(random_controls, random_controls_host);  // Ensure layouts match
     
+    // Create transform matrix view
     Kokkos::View<double**, MEMORY_SPACE> transform("transform", nsd, nsd);
     compute_transform_matrix(transform);
 
