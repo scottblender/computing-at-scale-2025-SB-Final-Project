18f882064c2858ec820f911a9ee131ebeb0b03f2
Scott Blender <scottblender20@gmail.com>
2025-04-28 21:21:07 -0400

    revert

--- GPU/include/sample_controls_host.hpp
+++ GPU/include/sample_controls_host.hpp
@@ -2,6 +2,7 @@
 #define SAMPLE_CONTROLS_HOST_HPP
 
 #include <Kokkos_Core.hpp>
+#include "../include/kokkos_types.hpp"
 
 // Conditionally define memory space based on CUDA availability
 #ifdef KOKKOS_ENABLE_CUDA
@@ -13,7 +14,7 @@
 // Function to fill a Host matrix with random controls (standard normal samples)
 void sample_controls_host_host(
     int total_samples,
-    Kokkos::View<double**, MEMORY_SPACE>& random_controls_out
+    HostMatrix& random_controls_out
 );
 
 #endif // SAMPLE_CONTROLS_HOST_HPP
